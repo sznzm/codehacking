701502c878230277ccd007a7fc650ef3dc878a2c
Suzana <suzanizam87@gmail.com>
2021-08-02 20:28:02 +0200

    categories CRUD added

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -29,5 +29,7 @@ Route::group(['middleware'=>'admin'], function(){
 
     Route::resource('admin/posts', 'AdminPostsController');
 
+    Route::resource('admin/categories', 'AdminCategoriesController');
+
 });
 
