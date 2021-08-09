f17ecb5775965d29a45486f5a3abbd07cf36a6b7
Suzana <suzanizam87@gmail.com>
2021-08-09 21:58:18 +0200

    gravatar getter added

--- app/User.php
+++ app/User.php
@@ -49,4 +49,11 @@ class User extends Authenticatable
  
     }
 
+    public function getGravatarAttribute(){
+
+        $hash = md5(strtolower(trim($this->attributes['email']))) . "?d=mm";
+
+        return "http://www.gravatar.com/avatar/$hash";
+    }
+
 }
