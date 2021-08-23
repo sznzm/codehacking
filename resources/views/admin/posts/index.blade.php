0df18903735aa88c6bf37b4ad9a1545ae4c1637f
Suzana <suzanizam87@gmail.com>
2021-08-23 21:49:40 +0200

    update laravel 5.5

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -29,7 +29,7 @@
                 <td>{{$post->user->name}}</td>
                 <td>{{$post->category ? $post->category->name : 'Uncategorized'}}</td>
                 <td><a href="{{route('admin.posts.edit', $post->id)}}">{{$post->title}}</a></td>
-                <td>{{str_limit($post->body, 30)}}</td>
+                <td>{!! str_limit($post->body, 30) !!}</td>
                 <td><a href="{{route('home.post', $post->id)}}">View post</a></td>
                 <td><a href="{{route('admin.comments.show', $post->id)}}">View comments</a></td>
                 <td>{{$post->created_at->diffForHumans()}}</td>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -12,10 +12,10 @@
     <title>Admin</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="{{asset('css/app.css')}}" rel="stylesheet">
 
     <link href="{{asset('css/libs.css')}}" rel="stylesheet">
 
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
